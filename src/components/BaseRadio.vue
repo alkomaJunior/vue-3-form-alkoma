4e0b169df5cb506d0f14bae9d6c42377155ba138
Aimé ALOGNIKOU <48490387+alkomaJunior@users.noreply.github.com>
2021-12-19 14:26:19 +0100

    Building BaseRadio component part 1.

---
+++ src/components/BaseRadio.vue
@@ -0,0 +1,30 @@
+<template>
+  <input
+    type="radio"
+    :checked="modelValue === value"
+    :value="value"
+    @change="$emit('update:modelValue', value)"
+    v-bind="$attrs"
+  />
+  <label v-if="label">Yes</label>
+</template>
+
+<script>
+export default {
+  name: 'BaseRadio',
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+    modelValue: {
+      type: [String, Number],
+      default: '',
+    },
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
